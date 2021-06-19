c1839f3a274ba7134903e47acec1bd9917734805
scriblemelego <keiferms3@gmail.com>
2021-06-19 02:28:00 -0400

    Program should be about complete now, fully implimented the tallying and modification of rolls along with displaying of the rolls. Should be pretty much crash-proof now too, as hard as I try I can't seem to get it to crash.

--- DiceRoller/DiceRoller.cpp
+++ DiceRoller/DiceRoller.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 //Prototypes
 void diceInputAnalyzer(const string& input, int& amount, int& sides, int& modifier, char& sign);
+int tallyAndModify(vector<int> results, int modifier, char sign);
 
 int main()
 {
@@ -18,6 +19,10 @@ int main()
 	vector<int> results;
 	
 	do {
+		//Clearing Variables
+		amount = 0, sides = 0, modifier = 0, sign = ' ';
+		results.clear();
+
 		//Input Gathering
 		cout << "Enter a roll: ";
 		getline(cin, input);
@@ -25,15 +30,21 @@ int main()
 		if (input != "stop") {
 			//Input Analysis
 			diceInputAnalyzer(input, amount, sides, modifier, sign);
-
+			
 			//Rolling
 			results = dice.rollMultiple(amount, sides);
 
 			//Display Results
-			for (int i = 0; i < amount; ++i)
-				cout << results[i] << " ";
-			cout << endl;
-			results.clear();
+			if (sign != ' ')
+				cout << "\nResult of " << amount << "d" << sides << " " << sign << " " << modifier << ": " << tallyAndModify(results, modifier, sign) << endl;
+			else
+				cout << "\nResult of " << amount << "d" << sides << ": " << tallyAndModify(results, modifier, sign) << endl;
+			cout << "-------------------------\n";
+			for (int i = 0; i < amount - 1; ++i)
+				cout << results[i] << ", ";
+			if (amount > 0)
+			cout << results[amount - 1] << endl << endl;
+
 		}
 	} while (input != "stop");
 }
@@ -42,9 +53,7 @@ void diceInputAnalyzer(const string& input, int& amount, int& sides, int& modifi
 {
 	vector<char> charInts;
 	int size = input.size(), size2 = 0, intCast = 0;
-	bool extra = false;
-
-	amount = 0, sides = 0, modifier = 0;
+	bool extra = false, d = false;
 
 	for (int i = 0; i < size; ++i)
 	{
@@ -68,11 +77,13 @@ void diceInputAnalyzer(const string& input, int& amount, int& sides, int& modifi
 			}
 			charInts.clear();
 			size2 = 0;
+			d = true;
 		}
 		//Looks for an operator to determine whether the extra section is necessary
 		//and then stores the sides chars as an int
 		else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
 		{
+			if (d == true) {
 			sign = input[i];
 			for (int j = 0; j < size2; ++j)
 			{
@@ -81,7 +92,7 @@ void diceInputAnalyzer(const string& input, int& amount, int& sides, int& modifi
 			}
 			charInts.clear();
 			size2 = 0;
-			extra = true;
+			extra = true; }
 		}
 	}
 	//Stores sides chars as an int if there's no operator
@@ -104,3 +115,34 @@ void diceInputAnalyzer(const string& input, int& amount, int& sides, int& modifi
 	}
 }
 
+int tallyAndModify(vector<int> results, int modifier, char sign)
+{
+	int size = results.size();
+	int total = 0;
+
+	for (int i = 0; i < size; ++i)
+		total += results[i];
+
+	switch (sign) {
+
+	case '+':
+		total += modifier;
+		break;
+
+	case '-':
+		total -= modifier;
+		break;
+
+	case '*':
+		total *= modifier;
+		break;
+
+	case '/':
+		total /= modifier;
+		break;
+
+	case ' ':
+		break;
+	}
+	return total;
+}
